e97afc8415356c7c112c01fca901bed9ebc971bb
saintEvol <casle_death@163.com>
2025-11-07 13:21:32 +0800

    增加UNIX_EPOCH的定义

--- types/src/time.rs
+++ types/src/time.rs
@@ -1,8 +1,11 @@
 #[cfg(not(target_arch = "wasm32"))]
 pub mod time {
-    pub type Instant = std::time::Instant;
-    pub type Duration = std::time::Duration;
-    pub type SystemTime = std::time::SystemTime;
+    use std::time;
+
+    pub type Instant = time::Instant;
+    pub type Duration = time::Duration;
+    pub type SystemTime = time::SystemTime;
+    pub const UNIX_EPOCH: SystemTime = time::UNIX_EPOCH;
 }
 
 #[cfg(target_arch = "wasm32")]
@@ -10,4 +13,5 @@ pub mod time {
     pub type Instant = web_time::Instant;
     pub type SystemTime = web_time::SystemTime;
     pub type Duration = std::time::Duration;
+    pub const UNIX_EPOCH: web_time::SystemTime = web_time::UNIX_EPOCH;
 }
