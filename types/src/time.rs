23c9f5d697dd8dcb0324e381306cf2d8d1b53574
saintEvol <casle_death@163.com>
2025-11-07 12:45:34 +0800

    增加SystemTime的跨平台类型

--- types/src/time.rs
+++ types/src/time.rs
@@ -2,10 +2,12 @@
 pub mod time {
     pub type Instant = std::time::Instant;
     pub type Duration = std::time::Duration;
+    pub type SystemTime = std::time::SystemTime;
 }
 
 #[cfg(target_arch = "wasm32")]
 pub mod time {
     pub type Instant = web_time::Instant;
+    pub type SystemTime = web_time::SystemTime;
     pub type Duration = std::time::Duration;
 }
